a857dc00545d08173c7528692d7a399fc26b4720
VoBerlin <105406765+VoBerlin@users.noreply.github.com>
2022-07-09 00:40:52 +0700

    PLL updated

--- MPC_MG_script_raw_dmpc.m
+++ MPC_MG_script_raw_dmpc.m
@@ -93,10 +93,10 @@ Smax = [45e3 45e3 37e3 37e3]';
 Tsamp = 0.1; %secondary
 
 lo_co = 0.1; %PQ low pass filter
-time_thresh = 0.5;
+time_thresh = 1;
 
 %horizon step
-N=5;
+N=3;
 %save mat file
 save('init.mat');
 
